2aaa72fcdf70c4237720e9c4dda57065384cca70
Benjamin Hase <benjamin.hase@web.de>
2012-09-27 19:52:40 +0200

    add test case for C4 max

--- tests/HP03STest.cpp
+++ tests/HP03STest.cpp
@@ -301,6 +301,14 @@ TEST(HP03S_Coefficients, C4Min)
 	LONGS_EQUAL(9734, HP03S_GetPressure());
 }
 
+TEST(HP03S_Coefficients, C4Max)
+{
+	testWithCoefficient(C4_TemperatureCoefficientOfOffset, 0x1000);
+
+	LONGS_EQUAL(-73, HP03S_GetTemperature());
+	LONGS_EQUAL(11445, HP03S_GetPressure());
+}
+
 /* replace the return values of ReadTemperature and ReadPressure */
 /* Replace the values of C1 - C7 and A - D */
 /* erroneous values? */
