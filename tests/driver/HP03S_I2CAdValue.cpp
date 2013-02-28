da0948f25eca43b508f19a48b46add57bebd4215
Benjamin Hase <benjamin.hase@web.de>
2013-02-28 06:38:05 +0100

    cleanup AD test

--- tests/driver/HP03S_I2CAdValue.cpp
+++ tests/driver/HP03S_I2CAdValue.cpp
@@ -22,17 +22,15 @@ TEST_GROUP(HP03S_AdValue)
 {
 	HP03S_Result result;
 	HP03S_Result expected_result;
-	uint8_t ad_command[2];
-	uint8_t expected_value[2];
 	uint16_t expected_ad_value;
 	uint16_t ad_value;
 
 	static const I2C_Address Ad_DeviceAddress = 0xEE;
+	static const uint8_t ChannelPressure = 0xF0;
+	static const uint8_t ChannelTemperature = 0xD0;
 
 	void setup(void)
 	{
-		expected_value[0] = 0x04;
-		expected_value[1] = 0x73;
 		ad_value = 0;
 		expected_ad_value = 0x473;
 
@@ -53,38 +51,51 @@ TEST_GROUP(HP03S_AdValue)
 		LONGS_EQUAL(expected_result, result);
 		LONGS_EQUAL(expected_ad_value, ad_value);
 	}
+
+	void setupCommon_AdValue_Read(uint8_t ad_channel)
+	{
+		/* expect: I2C start
+		 * I2C write (AD address, 0xFF, 0xF0)
+		 * => I2C_WriteTo(...)
+		 * => I2C_Run()
+		 * Delay 40 ms
+		 * I2C write (AD address, 0xFD)
+		 * => I2C_WriteTo(...)
+		 * read ad value
+		 * => I2C_readFrom(...)
+		 * => I2C_Run
+		 */
+
+		uint8_t ad_command[2];
+		uint8_t i2c_answer[2];
+
+		ad_command[0] = 0xFF;
+		ad_command[1] = ad_channel;
+
+		i2c_answer[0] = 0x04;
+		i2c_answer[1] = 0x73;
+
+		MockI2C_Expect_I2C_WriteTo_and_check_buffer(Ad_DeviceAddress,
+							    2, ad_command);
+		MockI2C_Expect_I2C_Run_and_return(I2C_Ok);
+
+		/* TODO this does not enforce order */
+		mock_c()->expectOneCall("Time_msWait")->withIntParameters("milliseconds", 40);
+
+		ad_command[0] = 0xFD;
+		MockI2C_Expect_I2C_WriteTo_and_check_buffer(Ad_DeviceAddress,
+							    1, ad_command);
+		MockI2C_Expect_I2C_ReadFrom_and_fill_buffer(Ad_DeviceAddress,
+							    2, i2c_answer);
+		MockI2C_Expect_I2C_Run_and_return(I2C_Ok);
+	}
+
 };
 
 
 TEST(HP03S_AdValue, PressureReadOk)
 {
-	/* expect: I2C start
-	 * I2C write (AD address, 0xFF, 0xF0)
-	 * => I2C_WriteTo(...)
-	 * => I2C_Run()
-	 * Delay 40 ms
-	 * I2C write (AD address, 0xFD)
-	 * => I2C_WriteTo(...)
-	 * read ad value
-	 * => I2C_readFrom(...)
-	 * => I2C_Run
-	 */
-	ad_command[0] = 0xFF;
-	ad_command[1] = 0xF0;
-
-	MockI2C_Expect_I2C_WriteTo_and_check_buffer(Ad_DeviceAddress,
-						    2, ad_command);
-	MockI2C_Expect_I2C_Run_and_return(I2C_Ok);
-
-	/* TODO this does not enforce order */
-	mock_c()->expectOneCall("Time_msWait")->withIntParameters("milliseconds", 40);
-
-	ad_command[0] = 0xFD;
-	MockI2C_Expect_I2C_WriteTo_and_check_buffer(Ad_DeviceAddress,
-						    1, ad_command);
-	MockI2C_Expect_I2C_ReadFrom_and_fill_buffer(Ad_DeviceAddress,
-						    2, expected_value);
-	MockI2C_Expect_I2C_Run_and_return(I2C_Ok);
+	setupCommon_AdValue_Read(ChannelPressure);
 
 	result = HP03S_ReadPressure(&ad_value);
 }
@@ -92,22 +103,7 @@ TEST(HP03S_AdValue, PressureReadOk)
 
 TEST(HP03S_AdValue, TemperatureReadOk)
 {
-	ad_command[0] = 0xFF;
-	ad_command[1] = 0xD0;
-
-	MockI2C_Expect_I2C_WriteTo_and_check_buffer(Ad_DeviceAddress,
-						    2, ad_command);
-	MockI2C_Expect_I2C_Run_and_return(I2C_Ok);
-
-	/* TODO this does not enforce order */
-	mock_c()->expectOneCall("Time_msWait")->withIntParameters("milliseconds", 40);
-
-	ad_command[0] = 0xFD;
-	MockI2C_Expect_I2C_WriteTo_and_check_buffer(Ad_DeviceAddress,
-						    1, ad_command);
-	MockI2C_Expect_I2C_ReadFrom_and_fill_buffer(Ad_DeviceAddress,
-						    2, expected_value);
-	MockI2C_Expect_I2C_Run_and_return(I2C_Ok);
+	setupCommon_AdValue_Read(ChannelTemperature);
 
 	result = HP03S_ReadTemperature(&ad_value);
 }
