a2f416a43e4f90a16c7a1c961f980ac7730b6d6d
Benjamin Hase <benjamin.hase@web.de>
2013-03-05 06:49:17 +0100

    documentation of MockI2C

--- mocks/MockI2C.h
+++ mocks/MockI2C.h
@@ -1,9 +1,26 @@
-#ifndef D_MockI2C_H
-#define D_MockI2C_H
+#ifndef D_MOCKI2C_H
+#define D_MOCKI2C_H
 
 /**********************************************************
  *
- * MockI2C is responsible for ...
+ * MockI2C is responsible for replacing real I2C
+ * interaction. It provides possibities to simulate
+ * successful transactions as well as errors.
+ *
+ * Usage: Create a mock with MockI2C_Create,
+ * feed expected input with MockI2C_Expect_*
+ * and run your code under test inside CppUTest.
+ * If your code behaves well, everything is fine.
+ * If not, you will get an assertion. To see,
+ * if your code made all expected calls, use
+ * MockI2C_CheckExpectations. A good place can
+ * be your teardown method ;-)
+ *
+ * Limitations: Currently there is a hard
+ * implementation of I2C_Run, I2C_WriteTo and I2C_ReadFrom
+ * in the mock so that real implementations will not
+ * be linked in. This is no problem for HP03S, which
+ * does not provide implementations for this API.
  *
  **********************************************************/
 
@@ -21,4 +38,4 @@ void MockI2C_Expect_I2C_WriteTo_and_check_buffer(I2C_Address device_address,
 void MockI2C_Expect_I2C_Run_and_return(I2C_Result result);
 
 void MockI2C_CheckExpectations(void);
-#endif  /* D_FakeMockI2C_H */
+#endif  /* D_MOCKI2C_H */
